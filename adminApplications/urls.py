afff03e5385892e9f611347b75e0df03eb697249
Julian Gil <juliangils@yahoo.com.co>
2014-07-10 18:52:06 -0500

    se agregaron todas las urls del proyecto

--- adminApplications/urls.py
+++ adminApplications/urls.py
@@ -14,4 +14,15 @@ urlpatterns = [
     # Activamos la url de los mediafiles
 
     url(r'^', 'adminApplications.views.inicio'),
+    url(r'^inicio', 'adminApplications.views.inicio'),
+    url(r'^newDevice', 'adminApplications.views.inicio'),
+    url(r'^newApplication', 'adminApplications.views.inicio'),
+    url(r'^allDevices', 'adminApplications.views.inicio'),
+    url(r'^allApplications', 'adminApplications.views.inicio'),
+    url(r'^oneDevice', 'adminApplications.views.inicio'),
+    url(r'^oneApplication', 'adminApplications.views.inicio'),
+    url(r'^installApplications', 'adminApplications.views.inicio'),
+    url(r'^deviceDetails', 'adminApplications.views.inicio'),
+    url(r'^applicationDetails', 'adminApplications.views.inicio'),
+    url(r'^register', 'adminApplications.views.inicio'),
 ]
