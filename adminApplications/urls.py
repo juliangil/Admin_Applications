c91981df2987f54c63036961257f2032ef12ceda
Julian Gil <juliangils@yahoo.com.co>
2014-07-10 19:44:19 -0500

    Agregando estilos a las plantillas

--- adminApplications/urls.py
+++ adminApplications/urls.py
@@ -13,15 +13,15 @@ urlpatterns = [
 
     # Activamos la url de los mediafiles
 
-    url(r'^inicio', 'adminApplications.views.inicio'),
-    url(r'^register', 'adminApplications.views.register'),
-    url(r'^newDevice', 'adminApplications.views.newDevice'),
-    url(r'^newApplication', 'adminApplications.views.newApplication'),
-    url(r'^allDevices', 'adminApplications.views.allDevices'),
-    url(r'^allApplications', 'adminApplications.views.allApplications'),
-    url(r'^oneDevice', 'adminApplications.views.oneDevice'),
-    url(r'^oneApplication', 'adminApplications.views.oneApplication'),
-    url(r'^installApplications', 'adminApplications.views.installApplications'),
-    url(r'^deviceDetails', 'adminApplications.views.deviceDetails'),
-    url(r'^applicationDetails', 'adminApplications.views.applicationDetails'),
+    url(r'^inicio/', 'adminApplications.views.inicio'),
+    url(r'^register/', 'adminApplications.views.register'),
+    url(r'^newDevice/', 'adminApplications.views.newDevice'),
+    url(r'^newApplication/', 'adminApplications.views.newApplication'),
+    url(r'^allDevices/', 'adminApplications.views.allDevices'),
+    url(r'^allApplications/', 'adminApplications.views.allApplications'),
+    url(r'^oneDevice/', 'adminApplications.views.oneDevice'),
+    url(r'^oneApplication/', 'adminApplications.views.oneApplication'),
+    url(r'^installApplications/', 'adminApplications.views.installApplications'),
+    url(r'^deviceDetails/', 'adminApplications.views.deviceDetails'),
+    url(r'^applicationDetails/', 'adminApplications.views.applicationDetails'),
 ]
