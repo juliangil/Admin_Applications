6f9f335951335c5e7e3cbeab918b8ccd2fd00720
Julian Gil <juliangils@yahoo.com.co>
2014-07-11 15:36:25 -0500

    Editando Modelos

--- adminDeviceApp/admin.py
+++ adminDeviceApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Device, Application, Tipo, deviceApp
 
-# Register your models here.
+admin.site.register(Device)
+admin.site.register(Application)
+admin.site.register(Tipo)
+admin.site.register(deviceApp)
--- adminDeviceApp/models.py
+++ adminDeviceApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Tipo(models.Model):
+	nombre = models.CharField(max_length=10, unique=True)
+
+	def __str__(self):
+		return self.nombre
+
+
+class Device(models.Model):
+	nombre = models.CharField(max_length=100, unique=True)
+	ip = models.CharField(max_length=12, unique=True)
+	estado = models.CharField(max_length=10)
+	tipo = models.ForeignKey(Tipo)
+
+	def __str__(self):
+		return self.nombre, self.estado
+
+
+class Application(models.Model):
+	nombre = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.nombre, self.estado
+
+
+class deviceApp(models.Model):
+	dispositivos = models.ForeignKey(Device)
+	aplicaciones = models.ForeignKey(Application)
+	estado = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.estado
